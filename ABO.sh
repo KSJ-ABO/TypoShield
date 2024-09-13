06420749595dab4784790da2937d6da50572d04c
hanbyeol325 <159513415+hanbyeol325@users.noreply.github.com>
2024-09-13 09:41:13 +0000

    Pending changes exported from your codespace

--- ABO.sh
+++ ABO.sh
@@ -22,33 +22,48 @@ INPUT=$1
 EXIST=$(apt-cache search "$INPUT" | grep -w "$INPUT")
 
 if [ -z "$EXIST" ]; then
-    echo "해당 패키지는 존재하지 않습니다."
-    exit 1
+   echo "해당 패키지는 존재하지 않습니다."
+   echo "존재하는 패키지가 맞습니까?"
+
+   read -p "Y/N: " OKAY
+   OKAY=$(echo "$OKAY" | tr '[:lower:]' '[:upper:]')
+   if [ "$OKAY" = "Y" ]; then
+      echo "해당 패키지를 대상으로 지정합니다"
+   else
+      echo "종료합니다."
+      exit 1
+   fi
 else
     echo "해당 패키지에 대한 오타 스쿼팅 여부를 판단합니다."
 fi
 
-CHECK=$(python3 cache.py "$INPUT")
+#CHECK=$(python3 cache.py "$INPUT")
 
-if [ $CHECK -eq 0 ]; then
-    RESULT=$(python3 ./func/test.py "$INPUT") # 머신러닝에 입력 값 삽입
-    RESULT_ARRAY=($RESULT) # 결과 순서가 (test, 유사도, 1)인 경우로 가정
+#if [ $CHECK -eq 0 ]; then
+#    RESULT=$(python3 ./func/test.py "$INPUT") # 머신러닝에 입력 값 삽입
+#    RESULT_ARRAY=($RESULT) # 결과 순서가 (test, 유사도, 1)인 경우로 가정
+#    
+#    ACCURACY=${RESULT_ARRAY[0]}
+#    NAME=${RESULT_ARRAY[1]}
+#    STATE=${RESULT_ARRAY[2]}
+#    python3 insert_cache.py "$INPUT", "$ACCURACY", "$NAME"
+#    
+#else
+#    DATA=$(python3 cache.py "$INPUT")
+#    ACCURACY=${DATA[0]}
+#    NAME=${DATA[1]}
+#    STATE=${DATA[2]}
     
-    ACCURACY=${RESULT_ARRAY[0]}
-    NAME=${RESULT_ARRAY[1]}
-    STATE=${RESULT_ARRAY[2]}
-    python3 insert_cache.py "$INPUT", "$ACCURACY", "$NAME"
-    
-else
-    DATA=$(python3 cache.py "$INPUT")
-    ACCURACY=${DATA[0]}
-    NAME=${DATA[1]}
-    STATE=${DATA[2]}
-    
+RESULT=$(python3 ./func/test.py "$INPUT") # 머신러닝에 입력 값 삽입
+RESULT_ARRAY=($RESULT) # 결과 순서가 (test, 유사도, 1)인 경우로 가정
+   
+ACCURACY=${RESULT_ARRAY[0]}
+NAME=${RESULT_ARRAY[1]}
+STATE=${RESULT_ARRAY[2]}
 
 if [ "$STATE" -eq 0 ]; then
    echo "$INPUT 에서 오타스쿼팅 확률이 발견되지않았습니다. 패키지를 다운 받습니다."
-   sudo apt-get install -y $INPUT
+   sudo apt-get install -y "$INPUT"
    if [$? -eq 0]; then
       echo "$INPUT 패키지 설치 완료."
    else
@@ -58,8 +73,9 @@ elif [ "$STATE" -eq 1 ]; then
    echo "해당 패키지는 $NAME 과 $ACCURACY의 유사도가 판별되었습니다. 오타스쿼팅일 가능성이 존재합니다. $INPUT을 다시 한번 확인해보시길 바랍니다"
    echo "의도한 패키지가 맞다면 설치를 계속하시겠습니까? (Y/N)"
    read -p "Y/N: " OKAY
-   if [ "$OKAY" -eq "Y" ]; then
-      sudo apt-get install -y $INPUT
+   OKAY=$(echo "$OKAY" | tr '[:lower:]' '[:upper:]')
+   if [ "$OKAY" = "Y" ]; then
+      sudo apt-get install -y "$INPUT"
       if [ $? -eq 0 ]; then
          echo "$INPUT 패키지 설치 완료."
       else
@@ -72,9 +88,9 @@ elif [ "$STATE" -eq 2 ]; then
    echo "$INPUT 패키지는 오타스쿼팅 패키지일 확률이 낮지만, 유사한 이름의 패키지가 존재합니다. 원하는 패키지가 '$INPUT'이 맞습니까?"
    echo "의도한 패키지가 맞다면 설치를 계속하시겠습니까? (Y/N)"
    read -p "Y/N: " OKAY
-   echo "$OKAY"
+   OKAY=$(echo "$OKAY" | tr '[:lower:]' '[:upper:]')
    if [ "$OKAY" = "Y" ]; then
-       sudo apt-get install -y $INPUT
+       sudo apt-get install -y "$INPUT"
       if [ $? -eq 0 ]; then
           echo "$INPUT 패키지 설치 완료."
       else

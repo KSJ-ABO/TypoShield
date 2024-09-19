4acf5ebcc48d6d9493c44871e9cdb319bc128b08
hanbyeol325 <159513415+hanbyeol325@users.noreply.github.com>
2024-09-19 23:56:28 +0900

    Update get_len.py

--- func/get_len.py
+++ func/get_len.py
@@ -7,8 +7,9 @@ data = ast.literal_eval(data)
 
 row_count = len(data)
 for package, packages in data:
-    print("--------------------------------------")
+    print("-------------------------------------------")
     print(f"package: {package}")
+    print("-------------------------------------------")
     for i, item in enumerate(packages):
         if i >= 4:
             break

1b8aa428748ac7d69e5973cd086b8e9c0cdefaa2
hanbyeol325 <159513415+hanbyeol325@users.noreply.github.com>
2024-09-19 23:56:02 +0900

    Update get_len.py

--- func/get_len.py
+++ func/get_len.py
@@ -1,12 +1,16 @@
 import ast
 import sys
 
+
 data = sys.argv[1]
 data = ast.literal_eval(data)
 
 row_count = len(data)
 for package, packages in data:
+    print("--------------------------------------")
     print(f"package: {package}")
-    for item in packages:
+    for i, item in enumerate(packages):
+        if i >= 4:
+            break
         print(f"  Name: {item[0]}, Similarity: {item[1]}")
 

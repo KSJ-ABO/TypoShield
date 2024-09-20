d977be8909c1b0983070014be76f9bdc3dc6e96c
hanbyeol325 <159513415+hanbyeol325@users.noreply.github.com>
2024-09-20 13:14:24 +0900

    Update ABO.sh

--- ABO.sh
+++ ABO.sh
@@ -119,20 +119,22 @@ if $option_p; then
        elif [ "$OKAY" = "N" ]; then
            echo "if you want to install package"
            read -p "Select Option: " CHOICE
-           if [ "$CHOICE" -eq 1 ]; then 
-               sudo apt-get install -y "$NAME"
+           if [ "$CHOICE" -eq 1 ]; then
+               TARGET_PACKAGE = "$NAME"
            elif [ "$CHOICE" -eq 2 ]; then
-               sudo apt-get install -y "${RESULT_ARRAY[4]}"
+               TARGET_PACKAGE = "${RESULT_ARRAY[4]}"
            elif [ "$CHOICE" -eq 3 ]; then
-               sudo apt-get install -y "${RESULT_ARRAY[6]}"
+               TARGET_PACKAGE = "${RESULT_ARRAY[6]}"
            else
                echo "NO Number"
            fi
+           echo "Download Packge: $TARGET_PACKAGE"
+           sudo apt-get install -y "$TARGET_PACKGE"
            echo ""
            if [ $? -eq 0 ]; then
-              echo "$INPUT Package Installation Completed"
+              echo "$TARGET_PACKGE Package Installation Completed"
           else
-             echo "$INPUT Package Installation Failed"
+             echo "$TARGET_PACKGE Package Installation Failed"
           fi
        else   
           exit 1
@@ -161,25 +163,21 @@ if $option_p; then
            echo "if you want to install package"
            read -p "Select Option: " CHOICE
            if [ "$CHOICE" -eq 1 ]; then
-               echo "Download $NAME"
-               echo ""
-               sudo apt-get install -y "$NAME"
+               TARGET_PACKAGE = "$NAME"
            elif [ "$CHOICE" -eq 2 ]; then
-               echo "Download ${RESULT_ARRAY[4]}"
-               echo ""
-               sudo apt-get install -y "${RESULT_ARRAY[4]}"
+               TARGET_PACKAGE = "${RESULT_ARRAY[4]}"
            elif [ "$CHOICE" -eq 3 ]; then
-               echo "Download ${RESULT_ARRAY[6]}"
-               echo ""
-               sudo apt-get install -y "${RESULT_ARRAY[6]}"
+               TARGET_PACKAGE = "${RESULT_ARRAY[6]}"
            else
-               echo "NO Number"
+               echo "System finish"
            fi
+           echo "Download Packge: $TARGET_PACKAGE"
+           sudo apt-get install -y "$TARGET_PACKGE"
            echo ""
            if [ $? -eq 0 ]; then
-              echo "$INPUT Package Installation Completed"
+              echo "$TARGET_PACKGE Package Installation Completed"
           else
-             echo "$INPUT Package Installation Failed"
+             echo "$TARGET_PACKGE Package Installation Failed"
           fi 
        else
           echo "DONE."

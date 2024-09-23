f792f492e03a2115623c3ff19c830b16a177aa0d
hanbyeol325 <159513415+hanbyeol325@users.noreply.github.com>
2024-09-23 09:27:41 +0900

    Update test.py

--- func/test.py
+++ func/test.py
@@ -17,9 +17,12 @@ array3 =list[2]
 name3 = array3[0]
 accuracy3 = array3[1]
 
-if accuracy == 1:
-    print("%2lf %s %d" % (accuracy, name, 0))
-elif accuracy > 0.9 :
-    print("%2lf %s %d %2lf %s %2lf %s" % (accuracy, name, 1, accuracy2, name2, accuracy3, name3))
+if accuracy:
+    if accuracy == 1:
+        print("%2lf %s %d" % (accuracy, name, 0))
+    elif accuracy > 0.9 :
+        print("%2lf %s %d %2lf %s %2lf %s" % (accuracy, name, 1, accuracy2, name2, accuracy3, name3))
+    else:
+        print("%2lf %s %d %2lf %s %2lf %s" % (accuracy, name, 2, accuracy2, name2, accuracy3, name3))
 else:
-    print("%2lf %s %d %2lf %s %2lf %s" % (accuracy, name, 2, accuracy2, name2, accuracy3, name3))
+    print("%d", 0)

e9dda852de98ad23c1203024b731789fa7af78bc
hanbyeol325 <159513415+hanbyeol325@users.noreply.github.com>
2024-10-12 18:56:54 +0900

    Update func.py

--- func/func.py
+++ func/func.py
@@ -1,47 +1,58 @@
 import sys
 import algo
 import algo3
-package = sys.argv[1]
-status = sys.argv[2]
 
-if status=="a":
-    list=algo.main(package)
-elif status == "p":
-    list=algo3.main(package)
+def check_package(package_name):
+    url = f"https://pypi.org/pypi/{package_name}"
+    response = requests.get(url)
 
-cnt = len(list)
-
-if cnt > 0:
-    array =list[0]
-    name =array[0]
-    accuracy=array[1]
-    if cnt >= 3:
-        array2 =list[1]
-        name2 =array2[0]
-        accuracy2=array2[1]
-        
-        array3 =list[2]
-        name3 = array3[0]
-        accuracy3 = array3[1]
-    elif cnt == 2:
-        array2 =list[1]
-        name2 =array2[0]
-        accuracy2=array2[1]
-
-        name3 = ""
-        accuracy3 = 0
-    elif cnt == 1:
-        name2 =""
-        accuracy2=0
-        
-        name3 = ""
-        accuracy3 = 0
-        
-    if accuracy == 1:
-        print("%2lf %s %d" % (accuracy, name, 0))
-    elif accuracy > 0.9 :
-        print("%2lf %s %d %2lf %s %2lf %s" % (accuracy, name, 1, accuracy2, name2, accuracy3, name3))
+    if response.status_code == 200:
+        print "exist"
     else:
-        print("%2lf %s %d %2lf %s %2lf %s" % (accuracy, name, 2, accuracy2, name2, accuracy3, name3))
-else:
-    print("%2lf %s %d" %(0,"",0))
+        print "None"
+        
+def main():
+    package = sys.argv[1]
+    status = sys.argv[2]
+    
+    if status=="a":
+        list=algo.main(package)
+    elif status == "p":
+        list=algo3.main(package)
+    
+    cnt = len(list)
+    
+    if cnt > 0:
+        array =list[0]
+        name =array[0]
+        accuracy=array[1]
+        if cnt >= 3:
+            array2 =list[1]
+            name2 =array2[0]
+            accuracy2=array2[1]
+            
+            array3 =list[2]
+            name3 = array3[0]
+            accuracy3 = array3[1]
+        elif cnt == 2:
+            array2 =list[1]
+            name2 =array2[0]
+            accuracy2=array2[1]
+    
+            name3 = ""
+            accuracy3 = 0
+        elif cnt == 1:
+            name2 =""
+            accuracy2=0
+            
+            name3 = ""
+            accuracy3 = 0
+            
+        if accuracy == 1:
+            print("%2lf %s %d" % (accuracy, name, 0))
+        elif accuracy > 0.9 :
+            print("%2lf %s %d %2lf %s %2lf %s" % (accuracy, name, 1, accuracy2, name2, accuracy3, name3))
+        else:
+            print("%2lf %s %d %2lf %s %2lf %s" % (accuracy, name, 2, accuracy2, name2, accuracy3, name3))
+    else:
+        print("%2lf %s %d" %(0,"",0))
